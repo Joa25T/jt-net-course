b1cf380e004c4fbef1fdc57a13a57e301356fbe6
Joaquin Telleria <jtelleriat@outlook.com>
2024-02-28 12:04:03 -0600

    Writing and readding from JSON

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Dapper;
+using HelloWorld.Data;
+using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Computer myComp = new Computer()
+            {
+                Motherboard ="Inteli7",
+                CPUCores = 12,
+                HasWifi = true,
+                ReleaseDate = DateTime.Now,
+                Price = 99.99m,
+                VideoCard = "Nvidia OrSOMT"
+
+            };
+
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard,
+                CPUCores,
+                HasWifi,
+                ReleaseDate,
+                Price,
+                VideoCard
+                ) VALUES ('" + myComp.Motherboard 
+                     + "','" + myComp.CPUCores
+                     + "','" + myComp.HasWifi
+                     + "','" + myComp.ReleaseDate
+                     + "','" + myComp.Price
+                     + "','" + myComp.VideoCard + "') \n";
+
+            //File.WriteAllText("log.txt", sql);
+            using StreamWriter openfile = new ("log.txt",append : true);
+
+            openfile.WriteLine(sql);
+
+        }
+    }
+
+}
